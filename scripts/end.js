a7950f77711516de85cdbd5a8b246f1f06cb1662
R-winDev <lowmrj@gmail.com>
2025-09-02 18:46:42 +0330

    high-scores

--- scripts/end.js
+++ scripts/end.js
@@ -3,6 +3,10 @@ const saveScoreBtn = document.getElementById("saveCoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", function () {
@@ -10,6 +14,17 @@ username.addEventListener("keyup", function () {
 });
 
 function saveHighScore(e) {
-    console.log("clicked the saved button");
     e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(MAX_HIGH_SCORES);
+
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("/");
 }
